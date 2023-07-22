a7281b01d152473ea9b754170e52b6d404e6841e
jaeha kim <kimjh0069@naver.com>
2023-07-22 20:14:52 +0900

    add missing button

---
+++ ui_tool/src/atom/Button/ButtonSmartAll.tsx
@@ -0,0 +1,7 @@
+export const ButtonSmartAll = () => {
+  return (
+    <button className="w-[359px] h-[44px] bg-primary-950 text-grayscale-0 hover:bg-primary-700">
+      스마트올클래스 자세히보기
+    </button>
+  );
+};
