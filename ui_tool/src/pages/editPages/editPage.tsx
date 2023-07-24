e5bc1faa18229bbb0cb234f4e12d6830e6af042b
snowriceDD <yoons100457@gmail.com>
2023-07-24 16:29:37 +0900

    "Feat: ...ing - array in object에 data 집어넣기"

--- ui_tool/src/pages/editPages/editPage.tsx
+++ ui_tool/src/pages/editPages/editPage.tsx
@@ -1,20 +1,42 @@
 import { AdabtiveTab } from "@molecule/AdabtiveTab/AdabtiveTab";
 import { Outlet, useParams, useNavigate } from "react-router-dom"
-import { useEffect } from "react";
+import { useEffect, useState, createContext } from "react";
 
+interface EditPageContextType {
+    editPageData: Object;
+    setEditPageData: React.Dispatch<React.SetStateAction<Object>>;
+}
+
+export const EditPageContext = createContext<EditPageContextType|null>(null);
 export const EditPage = () => {
-
     const navigate= useNavigate();
     const {id} = useParams();
+    const initialState = {
+        key : id,
+        title: 'asd',
+        page: `/${id}`,
+        date: Date(),
+        layout : [],
+    }
+    
+    const [editPageData, setEditPageData] = useState<Object>(initialState)
 
     useEffect(() => {
         navigate(`/edit/${id}/desktop`, {replace: true})
     }, [navigate, id]);
-    
+
+    useEffect(() => {
+        editPageData
+        console.log('updated!', editPageData)
+    }, [editPageData])
+
     return (
         <>
+            <EditPageContext.Provider value={{editPageData, setEditPageData}}>
             <AdabtiveTab />
+            <p>{JSON.stringify(editPageData, null, 2)}</p>
             <Outlet />
+            </EditPageContext.Provider>
         </>
     )
 }
--- ui_tool/src/pages/editPages/editPageDesktop.tsx
+++ ui_tool/src/pages/editPages/editPageDesktop.tsx
@@ -1,15 +1,30 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { EditAddSelectDesign } from "@molecule/Edit/EditAddSelectDesign";
+import { useContext, useState } from "react";
+import { EditPageContext } from "@pages/editPages/editPage";
 
 export const EditPageDesktop = () => {
-    const {id} = useParams();
+  const { id } = useParams();
+  const editPageContext = useContext(EditPageContext);
+
+  if (!editPageContext) {
+    throw new Error(
+      "EditPageContext must be used within a EditPageContext.Provider"
+    );
+  }
+  const { editPageData, setEditPageData } = editPageContext;
+
+  const handleEdit = () => {
+    // setEditPageData({...editPageData, editPageData['newKey'] = 'newValue' });
+    setEditPageData({...editPageData, key: 'value'})
+  }
     return (
         <div className="">
             <h1>
                 id : {id}
             </h1>
             <p> 데스크톱 페이지 </p>
-            <EditAddSelectDesign onClick={() => console.log('click')}/>
+            <EditAddSelectDesign onClick={handleEdit}/>
         </div>
     )
 }
