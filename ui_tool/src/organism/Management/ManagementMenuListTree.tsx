95a5e889170c54542278d7ec633d39302f2e9aae
snowriceDD <yoons100457@gmail.com>
2023-08-01 20:33:13 +0900

    "Fix: data Memo & mapping"

--- ui_tool/src/organism/Management/ManagementMenuListTree.tsx
+++ ui_tool/src/organism/Management/ManagementMenuListTree.tsx
@@ -5,88 +5,42 @@ import {
 import { ListAddMenu } from '@molecule/List/ListAddMenu';
 import type { ListInnerData, treeDataProps } from 'types';
 import { useSelector } from 'react-redux';
-import { useLoaderData } from 'react-router-dom';
-
-// const treeData: treeDataProps[] = [
-//   {
-//     title: <ListParentsMenu title={'웅진부끄러'} key={'123'} />,
-//     isParent: true,
-//     key: '0-0',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 아기'} key={'123'} />,
-//     isParent: false,
-//     key: '0-0-0',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 도비'} key={'123'} />,
-//     isParent: false,
-//     key: '0-0-1',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 바비'} key={'123'} />,
-//     isParent: false,
-//     key: '0-0-2',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 켄'} key={'123'} />,
-//     isParent: false,
-//     key: '0-0-3',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 저기'} key={'0-0-4'}/>,
-//     isParent: false,
-//     key: '0-0-4',
-//   },
-//   {
-//     title: <ListParentsMenu title={'웅진부끄러'} key={'123'} />,
-//     isParent: true,
-//     key: '0-1',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 아기'} key={'123'} />,
-//     isParent: false,
-//     key: '0-1-0',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 도비'} key={'123'} />,
-//     isParent: false,
-//     key: '0-1-1',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 바비'} key={'123'} />,
-//     isParent: false,
-//     key: '0-1-2',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 켄'} key={'123'} />,
-//     isParent: false,
-//     key: '0-1-3',
-//   },
-//   {
-//     title: <ListChildrenMenu title={'웅진부끄러 저기'} key={'123'} />,
-//     isParent: false,
-//     key: '0-1-4',
-//   },
-// ];
+import { useMemo } from 'react';
 
 export const ManageMenuListTree = () => {
-  /**Server data 전역 상태로 가져오기 */
-  // const data = useSelector((state: any) => state.modal);
-  const resData:any = useLoaderData();
-  const treeData = (resData.resData.data);
+  const storeData = useSelector((state: any) => state.pagesinfo.data.navigations);
 
+  const dataMappingFlat = (storeData: any) => {    
+    const result: any[] = [];
+    storeData.forEach((data: any) => {
+      if (data.category.isParent) {
+        result.push(data.category);
+      }
+      if (data.category.children.length > 0) {
+        data.category.children.forEach((child: any) => {
+          result.push(child);
+        });
+      }
+    });
+    return result
+  }
+  const mappingData = useMemo(() => dataMappingFlat(storeData), [storeData]);
+  console.log(mappingData);
+  
+  
   return (
     <>
       <ListAddMenu />
       <div className="w-[1220px] h-auto p-8 rounded-[26px] bg-grayscale-50 border border-grayscale-200 border-dashed">
         <div className="flex flex-col items-end gap-2">
-          {treeData.map(({ key, title, isParent, path, category, date }:ListInnerData) => (
-            isParent ? (<ListParentsMenu key={key} title={title} path={path} category={category} date={date} isParent={isParent} />) : (<ListChildrenMenu key={key} title={title} path={path} category={category} date={date} isParent={isParent} />)
-          ))}
+          {mappingData.map((value) => (
+            (value.isParent) ? (<ListParentsMenu key={value.idx} title={value.name} path={value.children[0].path} category={value.name} date={value.date} isParent={value.isParent} />) : (<ListChildrenMenu key={value.idx} title={value.name} path={value.path} category={value.name} date={value.date} isParent={value.isParent} />))
+          )}
         </div>
-        
       </div>
     </>
   );
 };
+
+//ListInnerData 타입 지정 다시하기 0801
+//(<ListChildrenMenu key={key} title={title} path={path} category={category} date={date} isParent={isParent}
