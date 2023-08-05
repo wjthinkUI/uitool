e3e6a97afc9f91c42865679fc5f61496da8f6183
snowriceDD <yoons100457@gmail.com>
2023-08-05 16:56:50 +0900

    "Fix: event bubbling -> stopPropagation 입력"

--- ui_tool/src/organism/Edit/EditBlock.tsx
+++ ui_tool/src/organism/Edit/EditBlock.tsx
@@ -1,22 +1,25 @@
 import { EditAddBlockHereTop, EditAddBlockHereBottom } from "@molecule/Edit/EditAddBlockHere"
 import { EditGroupContainer } from "@molecule/Edit/EditGroupContainer"
 import { EditToolsBox } from "@molecule/Edit/EditToolsBox"
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 
 interface EditBlockProps {
     children: ReactNode
     onClickTop: () => void
     onClickBottom: () => void
+    index: number
 }
-export const EditBlock = ({children, onClickTop, onClickBottom}:EditBlockProps) => {
+export const EditBlock = ({children, onClickTop, onClickBottom, index}:EditBlockProps) => {
+    const [modal, setModal] = useState(false)
 
+console.log(index)
     return (
         <EditGroupContainer>
             <EditAddBlockHereTop onClick={onClickTop}/>
             <p className="w-[100%] h-auto text-center ">
                 {children}
             </p>
-            <EditToolsBox block_id={1}/>
+            <EditToolsBox block_id={index}/>
             <EditAddBlockHereBottom onClick={onClickBottom}/>
         </EditGroupContainer>
     )
