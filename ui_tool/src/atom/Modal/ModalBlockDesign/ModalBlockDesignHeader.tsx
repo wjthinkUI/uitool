e7afeffc35e30fc164f7b7265fec066ec66618b3
snowriceDD <yoons100457@gmail.com>
2023-08-05 16:57:09 +0900

    "Fix: stopPropagation"

--- ui_tool/src/atom/Modal/ModalBlockDesign/ModalBlockDesignHeader.tsx
+++ ui_tool/src/atom/Modal/ModalBlockDesign/ModalBlockDesignHeader.tsx
@@ -5,11 +5,17 @@ interface ModalBlockDesignHeaderProps {
 export const ModalBlockDesignHeader = ({
   onCancel,
 }: ModalBlockDesignHeaderProps) => {
+
+  console.log(onCancel)
+  const handleClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    onCancel()
+  }
   return (
     <div className="p-6 pl-10 pr-10 w-[1220px] h-20 bg-primary-950 rounded-t-[1.25rem] mb-11">
       <div className="flex justify-between ">
         <span className="text-h3 text-grayscale-0">블록 디자인 추가</span>
-        <CloseIcon className="pb-3 cursor-pointer" onClick={onCancel} />
+        <CloseIcon className="pb-3 cursor-pointer" onClick={handleClick} />
       </div>
     </div>
   );
