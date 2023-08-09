4a045932fe4d11bba677edd07a8af988039fa14e
snowriceDD <yoons100457@gmail.com>
2023-08-09 17:32:21 +0900

    "Fix : LayoutContent 모달 방식"

--- ui_tool/src/molecule/Modal/ModalBlockDesign/LayoutContent.tsx
+++ ui_tool/src/molecule/Modal/ModalBlockDesign/LayoutContent.tsx
@@ -3,15 +3,14 @@ import { ModalBlockDesignMediumBox } from '@atom/Modal/ModalBlockDesign/ModalBlo
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '@store/store';
 import { updateTypeAndContentLayout } from '@store/slice/sliceEditPage';
+import { blockModalToggle } from '@store/slice/sliceModalToggle';
 
 export const LayoutContent = ({ list, type }: any) => {
   const blockIndex = useSelector(
-    (state: RootState) => state.editPage.selectedBlockIndex
-  );
-  const modalCancel = useSelector(
-    (state: RootState) => state.sidebar.modalCancel
+    (state: RootState) => state.modalToggle.selectedBlockIndex
   );
   const dispatch = useDispatch<AppDispatch>();
+
   const handleDispatch = (contentLayout: number, type: string) => {
     console.log('contentLayout = ', contentLayout, 'type = ', type);
     dispatch(
@@ -21,7 +20,7 @@ export const LayoutContent = ({ list, type }: any) => {
         contentLayout,
       })
     );
-    modalCancel();
+    dispatch(blockModalToggle());
   };
 
   return (
